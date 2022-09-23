1f095f0a544bb9c1c7998062e08a7d1767eba0f2
zhanghuihuihuil <97384154+zhanghuihuihuil@users.noreply.github.com>
2022-09-24 02:03:10 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+function redElement(id){
+    var element = document.getElementById(id);
+    element.style.fontSize = '10px';
+}
+const developer = "紅";
+console.log(developer);
+for(var i=0;i<10;i++){
+    console.warn('警告: 测试警告'+i);
+}
+
