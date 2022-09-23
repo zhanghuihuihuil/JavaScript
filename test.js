d635eb56189db79cfaf476ac6618c1385da2a24f
zhanghuihuihuil <97384154+zhanghuihuihuil@users.noreply.github.com>
2022-09-24 02:05:49 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ function redElement(id){
     var element = document.getElementById(id);
     element.style.fontSize = '10px';
 }
-const developer = "紅";
+const developer = "紅是什么意思";
 console.log(developer);
 for(var i=0;i<10;i++){
     console.warn('警告: 测试警告'+i);
